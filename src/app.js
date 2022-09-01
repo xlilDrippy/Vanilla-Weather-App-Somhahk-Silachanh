481a91a91448377131e7c153a934844eb7efd61f
xlilDrippy <Christina.Silachanh@gmail.com>
2022-09-01 17:40:01 -0600

    Updating Icon

--- src/app.js
+++ src/app.js
@@ -22,15 +22,19 @@ function displayTemperature(response){
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000)
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "e9fb2a9a6df791c5c71eb9635363af89";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=e9fb2a9a6df791c5c71eb9635363af89&units=metric`;
+let city = "Sydney";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e9fb2a9a6df791c5c71eb9635363af89&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
