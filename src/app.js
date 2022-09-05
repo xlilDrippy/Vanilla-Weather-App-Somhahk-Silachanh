fd628d1ee4fa52b0b69df9ca25273cbe7644af2d
xlilDrippy <Christina.Silachanh@gmail.com>
2022-09-05 13:58:23 -0600

    Intergrating API forecast

--- src/app.js
+++ src/app.js
@@ -14,26 +14,37 @@ function formatDate(timestamp){
 
 }
 
+function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+}
+
 function displayForecast(response){
+
+    let forecast = response.data.daily; 
+
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
-    let days = ["Thu", "Fri", "Sat", "Sun"];
-    days.forEach(function(day){
+
+    forecast.forEach(function(forecastDay, index){
+        if (index <6){
     forecastHTML = forecastHTML + `
     <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-            src="http://openweathermap.org/img/wn/50d@2x.png"
+            src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
             alt=""
             width="42"/>
         <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> 18 </span>
-            <span class="weather-forecast-temperature-min"> 12 </span>
+            <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)} </span>
+            <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)} </span>
         </div>
     </div>
     `;
-    });
+    }});
 
     forecast = forecastHTML + `</div>`
     forecastElement.innerHTML = forecastHTML;
@@ -43,7 +54,6 @@ function getForecast(coordinates){
     let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=e9fb2a9a6df791c5c71eb9635363af89&units=metric`
     axios.get(apiURL).then(displayForecast);
 }
-
 
 
 
