2f8f076f438d1633c320cf45b23a191077bd0042
xlilDrippy <Christina.Silachanh@gmail.com>
2022-09-05 12:37:08 -0600

    Creating displayForecast

--- src/app.js
+++ src/app.js
@@ -14,6 +14,35 @@ function formatDate(timestamp){
 
 }
 
+function displaceForecast(){
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thu", "Fri", "Sat"];
+    days.forEach(function(day){
+    forecastHTML = forecastHTML + `
+    <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+            src="http://openweathermap.org/img/wn/50d@2x.png"
+            alt=""
+            width="42"/>
+        <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> 18 </span>
+            <span class="weather-forecast-temperature-min"> 12 </span>
+        </div>
+    </div>
+    `;});
+forecast = forecastHTML + `</div>`
+
+    forecastElement.innerHTML = forecastHTML;
+}
+
+
+
+
+
+
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -77,3 +106,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+displaceForecast();
